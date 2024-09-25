59ebf064011ef5b91513dd3064f7d21a65264faf
munashex <munashemugondaa@gmail.com>
2024-09-25 14:32:27 +0200

    resolving refresh problem

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,34 +1,30 @@
-import {useEffect} from 'react'; 
-import { useNavigate} from 'react-router-dom';
-
+import { useEffect } from 'react'; 
+import { useNavigate } from 'react-router-dom';
 
 const client_id = import.meta.env.VITE_APP_SPOTIFY_CLIENT_ID; 
-const redirects = import.meta.env.VITE_APP_SPOTIFY_REDIRECT_URI
+const redirects = import.meta.env.VITE_APP_SPOTIFY_REDIRECT_URI;
 
-
-const LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirects}&scope=user-read-private user-read-email user-top-read user-read-recently-played`;
-
-
+// Ensure the redirect URI is URL-encoded
+const LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${encodeURIComponent(redirects)}&scope=user-read-private user-read-email user-top-read user-read-recently-played`;
 
 function Login() { 
- const access_token = localStorage.getItem('access_token') 
-const navigate = useNavigate() 
-
+  const access_token = localStorage.getItem('access_token'); 
+  const navigate = useNavigate(); 
 
   useEffect(() => {
-  if(access_token) {
-   navigate('/profile')
-  }
-  }, [])
-
-
+    if (access_token) {
+      navigate('/profile');
+    }
+  }, [access_token, navigate]);
 
   return ( 
-    <div className="bg-[#121212] w-full h-screen  py-44 flex flex-col items-center gap-9">
-    <h1 className="text-white text-2xl font-bold lg:text-3xl">Login to Spotify</h1>
-    <a href={LOGIN_URL} className="bg-[#3BE477] hover:bg-[#3BE458] font-bold  py-2 px-9 text-lg  lg:text-xl rounded-full">Login</a>
+    <div className="bg-[#121212] w-full h-screen py-44 flex flex-col items-center gap-9">
+      <h1 className="text-white text-2xl font-bold lg:text-3xl">Login to Spotify</h1>
+      <a href={LOGIN_URL} className="bg-[#3BE477] hover:bg-[#3BE458] font-bold py-2 px-9 text-lg lg:text-xl rounded-full">
+        Login
+      </a>
     </div>
-  ) 
+  );
 }
 
 export default Login;
